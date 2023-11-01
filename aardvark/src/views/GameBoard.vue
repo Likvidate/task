4cb119b1a3a8426e9bf7fc98fefc241b403d07e2
Likvidate <irmis55555@gmail.com>
2023-11-01 16:38:57 +0200

    gameboard: add basic view

--- aardvark/src/views/GameBoard.vue
+++ aardvark/src/views/GameBoard.vue
@@ -1,25 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    {{ baseBoardConfiguration }}
+  <div>
+    {{ $store.state.baseBoardConfiguration }}
+    <roulette-component/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-interface BaseBoardConfiguration {
-  name: string
-  slots: number
-  results: string[]
-  colors: string[]
-  positionToId: number[]
-}
+import { mapActions } from 'vuex'
+import RouletteComponent from '@/components/RouletteComponent.vue'
 
 export default Vue.extend({
+  components: {
+    RouletteComponent
+  },
   data () {
     return {
-      baseBoardConfiguration: null as BaseBoardConfiguration | null
     }
   },
   computed: {
@@ -27,37 +23,18 @@ export default Vue.extend({
      * Computed property that gets the current API URL from the Vuex store.
      * @returns {string} The current API URL.
      */
-    apiUrl: {
-      get () {
-        return this.$store.state.apiUrl
-      }
+    apiUrl (): string {
+      return this.$store.state.apiUrl
+    },
+    baseBoardConfiguration () {
+      return this.$store.state.baseBoardConfiguration
     }
   },
   async created () {
-    this.baseBoardConfiguration = await this.loadBaseBoardConfiguration()
+    this.loadBaseBoardConfiguration()
   },
   methods: {
-    /**
-     * Fetches the base board configuration from the API.
-     * @returns {Promise<BaseBoardConfiguration>} A promise that resolves to the base board configuration.
-     * If the request fails or an error occurs, a default configuration is returned.
-     * Loading board here because it doesn't need to be constantly updated like other data.
-     */
-    async loadBaseBoardConfiguration (): Promise<BaseBoardConfiguration> {
-      try {
-        const response = await fetch(`${this.apiUrl}/configuration`)
-        if (response.ok) {
-          const data = await response.json()
-          return data
-        } else {
-          console.error('Failed to fetch data')
-          return { name: '', slots: 0, results: [], colors: [], positionToId: [] }
-        }
-      } catch (error) {
-        console.error('An error occurred:', error)
-        throw error
-      }
-    }
+    ...mapActions(['loadBaseBoardConfiguration'])
   }
 })
 </script>
