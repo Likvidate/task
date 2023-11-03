1fdd2610e590be4a664f3402a5e3cb5111e9ff43
Likvidate <irmis55555@gmail.com>
2023-11-03 13:49:51 +0200

    roulette-component: clean up code

--- aardvark/src/components/RouletteComponent.vue
+++ aardvark/src/components/RouletteComponent.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="roulette-container">
+    <!-- Roulette wheel container -->
     <div class="roulette-wheel">
+      <!-- Arrow shape -->
       <div class="arrow-shape">
+        <!-- Arrow character, can be replaced with an image or symbol -->
         &#9654;
       </div>
+      <!-- Roulette wheel -->
       <div class="wheel">
+        <!-- Loop through roulette results and create slots -->
         <div
           v-for="(result, index) in rouletteConfig.results"
           :key="index"
@@ -15,13 +20,13 @@
         </div>
       </div>
     </div>
-    <button @click="spinWheel" class="spin-button" :disabled="spinning">Spin</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+// Define the structure of the Roulette Configuration
 interface RouletteConfig {
   results: string[]
   colors: string[]
@@ -30,57 +35,36 @@ interface RouletteConfig {
 export default Vue.extend({
   data () {
     return {
-      spinning: false,
-      rotation: 0,
-      desiredResult: '30'
+      rotation: 0 // Initial rotation angle of the wheel
     } as {
-      spinning: boolean
       rotation: number
-      desiredResult: string
     }
   },
   computed: {
+    // Retrieve Roulette Configuration from the store
     rouletteConfig (): RouletteConfig {
       return this.$store.state.baseBoardConfiguration
+    },
+    // Get the current game result
+    currentGame (): string {
+      return this.$store.state.currentGame
+    }
+  },
+  watch: {
+    currentGame: {
+      handler (newGameResult) {
+        // Start spinning the wheel when a new game result is received
+        this.spinWheel(newGameResult.outcome)
+      },
+      deep: true
     }
   },
   methods: {
-    spinWheel () {
-      if (!this.spinning) {
-        this.spinning = true
-        const totalRotation = 10 * 360
-        const duration = 5000
-
-        if (!this.desiredResult || !this.rouletteConfig.results.includes(this.desiredResult)) {
-          // Invalid or missing desired result
-          this.spinning = false
-          return
-        }
-
-        const index = this.rouletteConfig.results.indexOf(this.desiredResult)
-        const slotAngle = 360 / this.rouletteConfig.results.length
-        const desiredRotation = totalRotation - (index * slotAngle + slotAngle / 2)
-
-        const interval = 100
-        const step = (totalRotation * interval) / duration
-        const stepCount = Math.ceil(desiredRotation / step)
-
-        let currentRotation = 0
-        let steps = 0
-
-        const spinInterval = setInterval(() => {
-          currentRotation += step
-          this.rotation = currentRotation
-
-          if (steps >= stepCount) {
-            clearInterval(spinInterval)
-            this.rotation = desiredRotation
-            this.spinning = false
-          }
-          steps++
-        }, interval)
-      }
-    },
+    /**
+     * Calculate the style for each slot based on its index.
+     * @param {number} index - The index of the slot.
+     * @returns {object} An object with CSS styles for the slot.
+     */
     slotStyle (index: number) {
       const slotAngle = 360 / this.rouletteConfig.results.length
       const rotate = `rotate(${index * slotAngle + this.rotation}deg)`
@@ -88,59 +72,97 @@ export default Vue.extend({
       // Adjusting the radius and spacing between slots
       const radius = '250px'
 
-      const textColor = this.rouletteConfig.colors[index] === 'black' ? 'white' : 'black' // Change the text color
+      // Determine text color based on background color
+      const textColor = this.rouletteConfig.colors[index] === 'black' ? 'white' : 'black'
 
       return {
         background: this.rouletteConfig.colors[index],
-        width: '40px', // Fixed width
-        height: '40px', // Fixed height
+        width: '40px',
+        height: '40px',
         transform: `${rotate} translate(${radius})`,
-        borderRadius: '50%', // Ensures the slot is circular
-        color: textColor // Set the text color
+        borderRadius: '50%',
+        color: textColor
       }
+    },
+    /**
+     * Spin the roulette wheel to the desired result.
+     * @param {string} desiredResult - The desired roulette wheel result.
+     */
+    spinWheel (desiredResult: string) {
+      const totalRotation = 10 * 360 // Total rotation angle for spinning
+      const duration = 2000 // 2 seconds for the animation
+      let startTime = 0
+
+      if (!desiredResult || !this.rouletteConfig.results.includes(desiredResult)) {
+        // Invalid or missing desired result, stop spinning
+        return
+      }
+
+      const index = this.rouletteConfig.results.indexOf(desiredResult)
+      const slotAngle = 360 / this.rouletteConfig.results.length
+      const desiredRotation = totalRotation - (index * slotAngle + slotAngle / 2)
+
+      const animate = (timestamp: number) => {
+        if (!startTime) {
+          startTime = timestamp
+        }
+        const elapsedTime = timestamp - startTime
+        const progress = Math.min(elapsedTime / duration, 1)
+
+        // Update the rotation angle based on animation progress
+        this.rotation = progress * desiredRotation
+
+        if (progress < 1) {
+          requestAnimationFrame(animate)
+        } else {
+          // Animation is complete
+        }
+      }
+
+      // Start the animation loop
+      requestAnimationFrame(animate)
     }
   }
 })
 </script>
 
 <style scoped>
-  .roulette-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
+.roulette-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+}
 
-  .roulette-wheel {
-    display: flex;
-    position: relative;
-  }
+.roulette-wheel {
+  display: flex;
+  position: relative;
+}
 
-  .arrow-shape {
-    position: absolute;
-    left: 300px; /* Adjust the left property to move the arrow closer to the wheel */
-    top: 35%;
-    transform: translate(-50%, -50%) rotate(180deg);
-    font-size: 24px;
-  }
+.arrow-shape {
+  position: absolute;
+  left: 300px;
+  top: 35%;
+  transform: translate(-50%, -50%) rotate(180deg);
+  font-size: 24px;
+}
 
-  .wheel {
-    position: relative;
-  }
+.wheel {
+  position: relative;
+  width: 80%; /* Adjust the width percentage to your preference */
+  padding-bottom: 80%; /* Maintain a square aspect ratio (1:1) for the wheel */
+}
 
-  /* Add styling for slot elements as needed */
-  .slot {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    user-select: none;
-    position: absolute;
-    transition: transform 5s ease;
-  }
-
-  .spin-button {
-    margin-top: 20px;
-  }
+.slot {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  user-select: none;
+  position: absolute;
+  transition: transform 5s ease; /* Smoothly transition slot rotations */
+  width: 10%; /* Adjust the width percentage to your preference */
+  height: 10%; /* Adjust the height percentage to your preference */
+}
 </style>
