3c9e44fdcc4b8b4f36bd6ecd28bb98399cb4cb5e
Likvidate <irmis55555@gmail.com>
2023-11-03 13:50:43 +0200

    index: remove unnecessary code

--- aardvark/src/router/index.ts
+++ aardvark/src/router/index.ts
@@ -11,12 +11,9 @@ const routes: Array<RouteConfig> = [
     component: GameBoard
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/statistic',
+    name: 'statistic',
+    component: () => import(/* webpackChunkName: "about" */ '../views/StatisticsView.vue')
   }
 ]
 
